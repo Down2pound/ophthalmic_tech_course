13fd652be1e0d4bc16ae8f37e30a821d4c8ff540
Jefchapin78 <98143531+Down2pound@users.noreply.github.com>
2026-07-20 16:46:58 -0400

    Route student portal pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
 import { Toaster } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import Certificate from "@/pages/Certificate";
+import CourseDashboard from "@/pages/CourseDashboard";
+import CourseModule from "@/pages/CourseModule";
+import JoinPractice from "@/pages/JoinPractice";
+import Login from "@/pages/Login";
 import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
 import Curriculum from "./pages/Curriculum";
 import EnrollmentSuccess from "./pages/EnrollmentSuccess";
+import Home from "./pages/Home";
 
 function Router() {
   return (
@@ -14,6 +19,11 @@ function Router() {
       <Route path="/" component={Home} />
       <Route path="/curriculum" component={Curriculum} />
       <Route path="/enrollment/success" component={EnrollmentSuccess} />
+      <Route path="/login" component={Login} />
+      <Route path="/join/:code" component={JoinPractice} />
+      <Route path="/course/module/:day" component={CourseModule} />
+      <Route path="/course/certificate" component={Certificate} />
+      <Route path="/course" component={CourseDashboard} />
       <Route path="/404" component={NotFound} />
       <Route component={NotFound} />
     </Switch>
